765a789ddbb66a56bfa9e4e07f430a00ad3f4d7f
Jose A. Molina <josemouribe@gmail.com>
2021-10-25 15:20:10 +0200

    feat(contact card): defining the needed props

--- src/components/contactCard/ContactCard.js
+++ src/components/contactCard/ContactCard.js
@@ -4,6 +4,10 @@ const ContactCard = () => {
   return (
     <div>
       <h2>Contact Card</h2>
+      <h3>Picture</h3>
+      <h3>Email</h3>
+      <h3>Name</h3>
+      <h3>Phone Number</h3>
     </div>
   )
 };
