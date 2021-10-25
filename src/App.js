29b21377752560b73f36878309a35c997da68600
Jose A. Molina <josemouribe@gmail.com>
2021-10-25 15:17:26 +0200

    feat(app.js): defining routes
    Dashboard and Contacts detail

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router';
 
 import NavBarComponent from './components/navBar/NavBar';
-import ContactCard from './components/contactCard/ContactCard';
+import Dashboard from './components/dashboard/Dashboard';
+import ContactCardDetail from './components/contactCardDetail/ContactCardDetail';
 
 const App = () => {
   return (
     <div >
-      <header>
-        < NavBarComponent />
-        <ContactCard />
-      </header>
+      <NavBarComponent />
+      <Switch>
+        <Route exact path="/" component={() => <Dashboard />}></Route>
+        <Route path="/contact" component={() => <ContactCardDetail />}></Route>
+      </Switch>
+
     </div>
   );
 }
