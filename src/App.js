d330e016ef763f3e6cede560a3d19ce3d23c8175
Jose A. Molina <josemouribe@gmail.com>
2021-10-25 12:32:54 +0200

    feat: initializing components
    NavBar and Contact components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import NavBarComponent from './components/navBarComponent/navBar';
+
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello this will be the contact list dashboard</h1>
+        < NavBarComponent />
       </header>
     </div>
   );
---
+++ src/components/navBarComponent/navBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navBar = () => {
+  return (
+    <div>
+      <h2>I will be the navigational bar component</h2>
+    </div>
+  )
+}
+
+export default navBar
