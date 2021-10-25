cd72147ff2578b51cd78ca98b185ca5cc6473fe3
Jose A. Molina <josemouribe@gmail.com>
2021-10-25 17:35:13 +0200

    feat(add.js): adding edit contact route and add contact

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import { Switch, Route } from 'react-router';
 import NavBarComponent from './components/navBar/NavBar';
 import Dashboard from './components/dashboard/Dashboard';
 import ContactCardDetail from './components/contactCardDetail/ContactCardDetail';
+import AddContact from './components/addContact/AddContact';
+import EditContact from './components/editContat/EditContact';
 
 const App = () => {
   return (
-    <div >
+    <div className="App">
       <NavBarComponent />
       <Switch>
         <Route exact path="/" component={() => <Dashboard />}></Route>
         <Route path="/contact" component={() => <ContactCardDetail />}></Route>
+        <Route path="/add" component={() => <AddContact />}></Route>
+        <Route path="/edit" component={() => <EditContact />}></Route>
       </Switch>
 
     </div>
