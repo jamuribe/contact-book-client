98361e948a39cbe6f65ad559589f2b2e0f69bccd
Jose A. Molina <josemouribe@gmail.com>
2021-10-25 16:32:58 +0200

    feat(contact card): rendering contacts information

--- src/components/contactCard/ContactCard.js
+++ src/components/contactCard/ContactCard.js
@@ -4,10 +4,14 @@ const ContactCard = () => {
   return (
     <div>
       <h2>Contact Card</h2>
-      <h3>Picture</h3>
-      <h3>Email</h3>
-      <h3>Name</h3>
-      <h3>Phone Number</h3>
+      <div className="card" style={{ "width": "18rem" }} >
+        <img className="card-img-top" src="https://thispersondoesnotexist.com/image" alt="Lets see the face" />
+        <div className="card-body">
+          <h5 className="card-title">Name</h5>
+          <p className="card-text">Email</p>
+          <p className="card-text">Phone Number</p>
+        </div>
+      </div>
     </div>
   )
 };
