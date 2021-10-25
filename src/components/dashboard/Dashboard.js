bde6b95e4e693a14f20a4cb2a8ad3096dea6a14d
Jose A. Molina <josemouribe@gmail.com>
2021-10-25 15:18:07 +0200

    feat(dashboard): creating Dashboard component

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import ContactCard from '../contactCard/ContactCard.js';
+
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>I guess I am dashboard</h1>
+      <ContactCard />
+    </div>
+  )
+}
+
+export default Dashboard
