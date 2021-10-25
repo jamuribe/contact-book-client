6168fd26ac50ee97ade3ab88d9aa80f34a897558
Jose A. Molina <josemouribe@gmail.com>
2021-10-25 15:18:44 +0200

    feat(contact card detail): creating contact card detail component

---
+++ src/components/contactCardDetail/ContactCardDetail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ContactCardDetail = () => {
+  return (
+    <div>
+      <h1>I am the card detail.</h1>
+      <h2>Update contact</h2>
+      <h2>Delete contact</h2>
+      <h2>Go to contacts</h2>
+    </div>
+  )
+}
+
+export default ContactCardDetail
