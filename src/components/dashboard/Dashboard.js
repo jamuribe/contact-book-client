96cce13d22948595b085d7b1282be833386f166e
Jose A. Molina <josemouribe@gmail.com>
2021-10-25 21:07:19 +0200

    feat(dashboard): defining classes

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 import ContactCard from '../contactCard/ContactCard.js';
 
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>I guess I am dashboard</h1>
-      <ContactCard />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12 my-5 text-right">
+          <Link to="/add" className="btn btn-outline-dark">Add new contact</Link>
+        </div>
+        <div className="col-md-6 mx-auto">
+
+          <h1>Dashboard</h1>
+          <ContactCard />
+        </div>
+      </div>
     </div>
   )
 }
