7dc615a231fa95ce306600aaf61327fbe91fecbf
Jose A. Molina <josemouribe@gmail.com>
2021-10-26 12:59:03 +0200

    feat(contact card detail): edit button
    Passing props to edit component

--- src/components/contactCardDetail/ContactCardDetail.js
+++ src/components/contactCardDetail/ContactCardDetail.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { useLocation } from 'react-router';
+import { useLocation, useHistory } from 'react-router';
 
 const ContactCardDetail = () => {
   const location = useLocation();
   const contact = location.state.contact;
+
+  const history = useHistory();
+
+  const handleButtonClick = (event) => {
+    history.push('/edit', { contact })
+  }
+
   return (
     <section className="container">
       <div className="row">
@@ -13,6 +20,8 @@ const ContactCardDetail = () => {
         >
           <img className="card-img-top rounded mx-auto" src="https://thispersondoesnotexist.com/image" alt="User" />
           <figcaption className="card-body">
+            <button className="col my-3 btn btn-outline-dark" onClick={handleButtonClick}> Edit contact
+            </button>
             <h5 className="card-title">{contact.contact.name}</h5>
             <p className="card-text">{contact.contact.email}</p>
             <p className="card-text">{contact.contact.number}</p>
