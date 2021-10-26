51b514d5dbde4bff0fc667d046c07437a0d3fa13
Jose A. Molina <josemouribe@gmail.com>
2021-10-26 18:59:50 +0200

    feat(contact card detail): history
    Adding history of recent edits

--- src/components/contactCardDetail/ContactCardDetail.js
+++ src/components/contactCardDetail/ContactCardDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation, useHistory } from 'react-router';
+import EditedHistory from '../editedHistory/editedHistory';
 
 const ContactCardDetail = () => {
   const location = useLocation();
@@ -12,23 +13,31 @@ const ContactCardDetail = () => {
   }
 
   return (
-    <section className="container">
+    <div className="container">
       <div className="row">
-        <figure
-          className="card shadow m-3 mx-auto"
-          style={{ "width": "25rem" }}
-        >
-          <img className="card-img-top rounded mx-auto" src="https://thispersondoesnotexist.com/image" alt="User" />
-          <figcaption className="card-body">
-            <button className="col my-3 btn btn-outline-dark" onClick={handleButtonClick}> Edit contact
-            </button>
-            <h5 className="card-title">{contact.contact.name}</h5>
-            <p className="card-text">{contact.contact.email}</p>
-            <p className="card-text">{contact.contact.number}</p>
-          </figcaption>
-        </figure>
+        <section className="col">
+          <figure
+            className="card shadow m-3"
+            style={{ "width": "25rem" }}
+          >
+            <img className="card-img-top rounded mx-auto" src="https://thispersondoesnotexist.com/image" alt="User" />
+            <figcaption className="card-body">
+              <button className="col my-3 btn btn-outline-dark" onClick={handleButtonClick}> Edit contact
+              </button>
+              <h5 className="card-title">{contact.contact.name}</h5>
+              <p className="card-text">{contact.contact.email}</p>
+              <p className="card-text">{contact.contact.number}</p>
+            </figcaption>
+          </figure>
+        </section>
+        <section className="col">
+          <h2 className="display-6 my-5">Last edits history</h2>
+          {
+            contact.contact.edited.map((contact) => < EditedHistory contact={contact} />)
+          }
+        </section>
       </div>
-    </section>
+    </div>
   )
 }
 
