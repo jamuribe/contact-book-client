fefd8499afab670c1ff9096f21b2f2d552f09d94
Jose A. Molina <josemouribe@gmail.com>
2021-10-26 15:07:19 +0200

    feat(reducers): delete reducer

--- src/redux/reducers/contactReducer.js
+++ src/redux/reducers/contactReducer.js
@@ -1,3 +1,5 @@
+// BRING INITIAL STATE FROM SERVER
+
 const initState = [{
   id: 1,
   name: 'Jose Antonio',
@@ -13,6 +15,10 @@ const contactReducer = (state = initState, action) => {
     case 'UPDATE_CONTACT':
       const updateState = state.map((contact) => contact.id === action.data.id ? action.data : contact)
       state = updateState;
+      return state;
+    case 'DELETE_CONTACT':
+      const filteredContacts = state.filter((contact) => contact !== action.data && contact);
+      state = filteredContacts;
       return state;
     default:
       return state;
