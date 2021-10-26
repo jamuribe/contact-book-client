687f1db74191c6362fd431cb6dbf01f793c1cec3
Jose A. Molina <josemouribe@gmail.com>
2021-10-26 10:13:27 +0200

    feat(dashboard): rendering contact cards in a list

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,22 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import ContactCard from '../contactCard/ContactCard.js';
 
 
 const Dashboard = () => {
+  const contacts = useSelector(state => state);
+
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-md-12 my-5 text-right">
-          <Link to="/add" className="btn btn-outline-dark">Add new contact</Link>
-        </div>
-        <div className="col-md-6 mx-auto">
-
-          <h1>Dashboard</h1>
-          <ContactCard />
-        </div>
+      <div className="col my-5 text-right">
+        <Link to="/add" className="btn btn-outline-dark">Add new contact</Link>
       </div>
+      <section class="row">
+        {
+          contacts.map((contact, id) => (
+            <ContactCard contact={contact} key={id} className="col-sm" />
+          ))
+        }
+      </section>
     </div>
   )
 }
