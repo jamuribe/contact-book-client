81aafcbc3e474b99e1229f03c4e7785c275cbe37
Jose A. Molina <josemouribe@gmail.com>
2021-10-26 10:55:21 +0200

    feat(contact card): handling click event

--- src/components/contactCard/ContactCard.js
+++ src/components/contactCard/ContactCard.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 
 const ContactCard = (contact) => {
+
+  const history = useHistory();
+
+  const handleClick = (event) => {
+    history.push('/contact', { contact })
+  }
+
   return (
-    <figure className="card shadow m-3" style={{ "width": "15rem" }} >
+    <figure
+      className="card shadow m-3"
+      style={{ "width": "15rem" }}
+      onClick={handleClick}
+    >
       <img className="card-img-top rounded mx-auto" src="https://thispersondoesnotexist.com/image" alt="User" />
       <figcaption className="card-body">
         <h5 className="card-title">{contact.contact.name}</h5>
