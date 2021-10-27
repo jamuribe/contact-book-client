307cb6919f70480fb99d13dfdca00ddb30e0be65
Jose A. Molina <josemouribe@gmail.com>
2021-10-27 21:25:42 +0200

    feat(api service): defining and implementing the different functions
    updateContact, getAllContacts, DeleteContact, addContact, createRandomContact

--- src/apiService/apiService.js
+++ src/apiService/apiService.js
@@ -1,9 +1,34 @@
-// GetAllContacts()
+import axios from "axios";
+import 'dotenv/config.js';
 
-// UpdateContact(contact)
+const url = process.env.URL;
 
-// DeleteOneContact(contact)
+export const getAllContacts = async () => {
+  let contact;
+  await axios.get('http://localhost:3001/')
+    .then((response) => {
+      contact = response.data
+    })
+  return contact;
+}
 
-// AddOneContact()
+export const updateContact = async (contact) => {
+  await axios.put(`${url}update`, contact)
+    .catch(error => console.log('Error 2: ', error))
+}
 
-// GenerateRandomContact()
+export const deleteContact = async (contact) => {
+  await axios.delete(`${url}delete`, contact)
+    .catch(error => console.log('Error 3: ', error))
+}
+
+export const addContact = async (contact) => {
+  await axios.post(`${url}add`, contact)
+    .catch(error => console.log('Error 4: ', error))
+}
+
+export const createRandomContact = async (id) => {
+  await axios.post(`${url}random`, id)
+    .catch(error => console.log('Error 4: ', error))
+
+}
