628a4498b1bb4007605d92c995d4eb7687dbe3ef
Jose A. Molina <josemouribe@gmail.com>
2021-10-28 22:02:56 +0200

    feat(app.js): connecting component to redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router';
+import { connect } from 'react-redux';
 
 import NavBarComponent from './components/navBar/NavBar';
-import Dashboard from './components/dashboard/Dashboard';
+import Dashboard from './containers/dashboard/Dashboard';
 import ContactCardDetail from './components/contactCardDetail/ContactCardDetail';
 import AddContact from './components/addContact/AddContact';
 import EditContact from './components/editContat/EditContact';
 
-const App = () => {
+const App = (props) => {
   return (
     <div className="App">
       <NavBarComponent />
       <Switch>
-        <Route exact path="/" component={() => <Dashboard />}></Route>
+        <Route exact path="/" component={() => <Dashboard props={props} />}></Route>
         <Route path="/contact" component={() => <ContactCardDetail />}></Route>
         <Route path="/add" component={() => <AddContact />}></Route>
         <Route path="/edit" component={() => <EditContact />}></Route>
@@ -23,4 +24,11 @@ const App = () => {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    contacts: state
+  }
+}
+
+export default connect(mapStateToProps)(App)
+
