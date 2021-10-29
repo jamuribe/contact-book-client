fff60ae18ef0dcd1f12f379f9a0e16c18e9836aa
Jose A. Molina <josemouribe@gmail.com>
2021-10-29 10:02:25 +0200

    feat(api service): implementing api service using dotenv

---
+++ src/apiService/apiService.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import 'dotenv/config.js';
+
+const url = process.env.REACT_APP_URL;
+console.log(url)
+
+export const getContactsAPI = async () => {
+  return await axios.get(url)
+    .catch(error => console.log('Error 1.0: ', error))
+}
+
+export const updateContactAPI = async (contact) => {
+  return await axios.put(`${url}update`, { data: { contact } })
+    .catch(error => console.log('Error 2.0: ', error))
+}
+
+export const deleteContactAPI = async (contact) => {
+  return await axios.delete(`${url}erase`, { data: { contact } })
+    .catch(error => console.log('Error 3.0: ', error))
+}
+
+export const addContactAPI = async (contact) => {
+  return await axios.post(`${url}add`, contact)
+    .catch(error => console.log('Error 4.0: ', error))
+}
+
+export const createRandomContactAPI = async () => {
+  return await axios.post(`${url}random`)
+    .catch(error => console.log('Error 5.0: ', error))
+
+}
--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -1,46 +1,45 @@
-import axios from 'axios';
-import 'dotenv/config.js';
 import { setContacts, addOne, randomContact, deleteContact, updateOne } from './actions';
-// const url = process.env.URL;
+import * as api from '../../apiService/apiService.js';
+
 
 const getAllContacts = () => async (dispatch) => {
-  await axios.get('http://localhost:3001/').then(response =>
+  await api.getContactsAPI().then(response =>
     dispatch(setContacts(response.data))
-  ).catch(error => console.log('Error 1: ', error));
+  ).catch(error => console.log('Error 1.1: ', error));
 };
 
 export const addContact = (contact) => async (dispatch) => {
-  await axios.post('http://localhost:3001/add', contact).then(response => {
+  await api.addContactAPI(contact).then(response => {
     dispatch(addOne(response.data));
   }).then(
     setTimeout(() => {
       dispatch(getAllContacts());
     }, 1000)
-  ).catch(error => console.log('Error 2: ', error));
+  ).catch(error => console.log('Error 4.1: ', error));
 };
 
 export const createRandomContact = () => async (dispatch) => {
-  await axios.post('http://localhost:3001/random').then(response => {
+  await api.createRandomContactAPI().then(response => {
     dispatch(randomContact());
   }).then(
     setTimeout(() => {
       dispatch(getAllContacts());
     }, 1000)
-  ).catch(error => console.log('Error 3: ', error));
+  ).catch(error => console.log('Error 5.1: ', error));
 };
 
 export const eraseContact = (contact) => async (dispatch) => {
-  await axios.delete('http://localhost:3001/erase', { data: { contact } }).then(response => {
+  await api.deleteContactAPI(contact).then(response => {
     dispatch(deleteContact(contact));
   }).then(
     dispatch(getAllContacts())
-  ).catch(error => console.log('Error 4: ', error));
+  ).catch(error => console.log('Error 3.1: ', error));
 };
 export const updateContact = (contact) => async (dispatch) => {
-  await axios.put('http://localhost:3001/update', { data: { contact } }).then(response => {
+  await api.updateContactAPI(contact).then(response => {
     dispatch(updateOne(contact));
   }).then((contact) => dispatch(getAllContacts())
-  ).catch(error => console.log('Error 2: ', error));
+  ).catch(error => console.log('Error 2.1: ', error));
 };
 
 export default getAllContacts;
